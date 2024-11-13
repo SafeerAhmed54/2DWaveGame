b26d98b5b5a1880eb63cde6be85ae43f2a022626
Safeer Ahmed <pakistanireacts9@gmail.com>
2024-11-13 13:33:51 +0500

    Added Reference
    Added Reference
    Value types

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private string _horizontalAxis = "Horizontal";
     [SerializeField] private string _verticalAxis = "Vertical";
-    [SerializeField] private int speed = 5;
+    [SerializeField] private int speed = 3;
     [SerializeField] private Rigidbody2D rb2DPlayer;
 
     private Vector2 _input;
@@ -18,14 +18,15 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb2DPlayer.velocity = _input;
+        rb2DPlayer.velocity = _input * speed;
     }
     // Update is called once per frame
     void Update()
     {
         float horizontalAxis = Input.GetAxisRaw(_horizontalAxis);
         float verticalAxis = Input.GetAxisRaw(_verticalAxis);
-        _input = new Vector2(horizontalAxis * speed * Time.deltaTime, verticalAxis * speed * Time.deltaTime);
-        
+        _input = new Vector2(horizontalAxis , verticalAxis);
+        _input.Normalize();
+
     }
 }
